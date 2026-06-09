08e1f7c7d862cfd20f81e3a8adc1e587e9126a47
Nurbu <lamanurbu2@gmail.com>
2026-06-09 14:49:51 -0400

    Add sneakers to the database as well

--- src/main/java/com/pluralsight/sneakerdrops/SneakerDrops4Application.java
+++ src/main/java/com/pluralsight/sneakerdrops/SneakerDrops4Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SneakerDrops4Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SneakerDrops4Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SneakerDrops4Application.class, args);
+    }
 
 }
--- src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
+++ src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
@@ -1,7 +1,9 @@
 package com.pluralsight.sneakerdrops;
 
 import com.pluralsight.sneakerdrops.data.BrandRepository;
+import com.pluralsight.sneakerdrops.data.SneakerRepository;
 import com.pluralsight.sneakerdrops.models.Brand;
+import com.pluralsight.sneakerdrops.models.Sneaker;
 import com.pluralsight.sneakerdrops.service.DropService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,11 +15,13 @@ public class StartupRunner implements CommandLineRunner {
 
     private final DropService dropService;
     private final BrandRepository brandRepository;
+    private final SneakerRepository sneakerRepository;
 
     @Autowired
-    public StartupRunner(DropService dropService, BrandRepository brandRepository) {
+    public StartupRunner(DropService dropService, BrandRepository brandRepository, SneakerRepository sneakerRepository) {
         this.dropService = dropService;
         this.brandRepository = brandRepository;
+        this.sneakerRepository = sneakerRepository;
 
     }
 
@@ -33,6 +37,14 @@ public class StartupRunner implements CommandLineRunner {
             brandRepository.save(new Brand("Adidas"));
             brandRepository.save(new Brand("New Balance"));
         }
+
+        if (sneakerRepository.count() == 0) {
+            sneakerRepository.save(new Sneaker("Jordans_1", 200, 1998));
+            sneakerRepository.save(new Sneaker("Jordans_4", 300, 2004));
+            sneakerRepository.save(new Sneaker("Jordans_11", 150, 2007));
+        }
         brandRepository.findAll().forEach(System.out::println);
+        sneakerRepository.findAll().forEach(System.out::println);
+        
     }
 }

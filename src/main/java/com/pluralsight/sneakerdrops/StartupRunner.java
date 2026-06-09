2596a3143ca0ed53831ba6c1cb4ead6082dab3b8
Nurbu <lamanurbu2@gmail.com>
2026-06-09 14:54:55 -0400

    Completed step 2 of asking user if they'd like to see the sneakers.

--- src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
+++ src/main/java/com/pluralsight/sneakerdrops/StartupRunner.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
 @Component
 
 public class StartupRunner implements CommandLineRunner {
@@ -16,19 +18,35 @@ public class StartupRunner implements CommandLineRunner {
     private final DropService dropService;
     private final BrandRepository brandRepository;
     private final SneakerRepository sneakerRepository;
+    private final Scanner scanner = new Scanner(System.in);
 
     @Autowired
     public StartupRunner(DropService dropService, BrandRepository brandRepository, SneakerRepository sneakerRepository) {
         this.dropService = dropService;
         this.brandRepository = brandRepository;
         this.sneakerRepository = sneakerRepository;
-
     }
 
     @Override
     public void run(String... args) {
         System.out.println(dropService.getStatus());
         seedData();
+
+        boolean istrue = true;
+        while (istrue) {
+            System.out.println("1: List all sneakers");
+            System.out.println("0: Quit");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    listSneakers();
+                case 0:
+                    istrue = false;
+            }
+        }
+    }
+
+    private void listSneakers() {
     }
 
     private void seedData() {
@@ -45,6 +63,6 @@ public class StartupRunner implements CommandLineRunner {
         }
         brandRepository.findAll().forEach(System.out::println);
         sneakerRepository.findAll().forEach(System.out::println);
-        
+
     }
 }
